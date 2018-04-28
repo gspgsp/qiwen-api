32ef33222463654bfe2df45993a4d7cf6fbe76b8
guoshipeng <guoshipeng@lansha.com>
2018-04-28 16:20:08 +0800

    奇闻家居api

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -123,7 +123,7 @@ class IndexController {
         $pc = new WxBizDataCryptService(env('WXAPP_ID'), $response['session_key']);
         $data = '';
         $errCode = $pc->decryptData($request->input('encryptedData'), $request->input('iv'), $data );
-        var_dump($data);
+        var_dump(json_decode($data, true));
         /*if ($errCode == 0) {
             print($data . "\n");
         } else {
