f30de8760075e037731970321978357fe2f7cbc6
guoshipeng <guoshipeng@lansha.com>
2018-04-28 13:12:04 +0800

    奇闻家居api

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -101,8 +101,8 @@ class IndexController {
             'js_code' => $request->input('code'),
             'grant_type' => env('GRANT_TYPE'),
         ];
-        $response = curl_request($api, 'GET', $params, [], "json");
-//        Log::debug('response_wx', ['response' => $response]);
+        $response = curl_request($api, 'GET', $params);
+        Log::debug('response_wx', ['response' => $response]);
         return response()->json([
             'status' => 200,
             'data' => $response,
