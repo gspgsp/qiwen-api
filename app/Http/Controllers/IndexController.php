536008b6ff9fbe0a3b8e5763f668b0b7205b9548
guoshipeng <guoshipeng@lansha.com>
2018-04-28 14:48:20 +0800

    奇闻家居api

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -104,7 +104,7 @@ class IndexController {
         ];
 
         // 使用curl_setopt()设置要获取的URL地址
-        $url = "https://api.weixin.qq.com/sns/jscode2session?&appid=".env('WXAPP_ID')."&secret=".env('WXAPP_SECRET')."&js_code=".$request->input('code')."&grant_type=".env('GRANT_TYPE');
+        $url = "https://api.weixin.qq.com/sns/jscode2session?appid=".env('WXAPP_ID')."&secret=".env('WXAPP_SECRET')."&js_code=".$request->input('code')."&grant_type=".env('GRANT_TYPE');
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_HEADER, 0 ); // 过滤HTTP头
         curl_setopt($curl,CURLOPT_RETURNTRANSFER, 1);// 显示输出结果
